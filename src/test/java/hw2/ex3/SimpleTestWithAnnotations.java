8c49a5bd3c43ea107f8fbff21db9633d624d01dc
MIXSTAYS <32819223+MIXSTAYS@users.noreply.github.com>
2018-05-27 09:51:54 +0300

    hw2 added

---
+++ src/test/java/hw2/ex3/SimpleTestWithAnnotations.java
@@ -0,0 +1,81 @@
+package hw2.ex3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+import static java.lang.System.setProperty;
+
+/**
+ * Created by Mikhail on Май, 2018
+ */
+public class SimpleTestWithAnnotations {
+    private WebDriver driver;
+    private long initTime;
+
+    @BeforeSuite
+    public void beforeSuite() {
+        initTime = System.currentTimeMillis();
+        setProperty("webdriver.chrome.driver", "chromedriver.exe");
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new ChromeDriver();
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println(driver.getTitle());
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.close();
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("Suite time is: " + (System.currentTimeMillis() - initTime));
+    }
+
+    @Test
+    public void simpleSeleniumTest() {
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("download.default_directory", "target");
+
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", chromePrefs);
+
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+        Assert.assertEquals(driver.getTitle(), "Home Page");
+
+        WebElement userIcon = driver.findElement(By.cssSelector(".profile-photo"));
+        userIcon.click();
+
+        driver.findElement(By.cssSelector("#Name")).sendKeys("epam");
+        driver.findElement(By.cssSelector("#Password")).sendKeys("1234");
+        driver.findElement(By.cssSelector(".form-horizontal button[type = 'submit']")).click();
+
+        WebElement userName = driver.findElement(By.cssSelector(".profile-photo span"));
+        Assert.assertTrue(userName.isDisplayed());
+        Assert.assertEquals(userName.getText(), "PITER CHAILOVSKII");
+    }
+}
