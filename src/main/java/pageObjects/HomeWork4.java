b48b274554f5b40cf782cf57600a8e72b97933a2
MIXSTAYS <32819223+MIXSTAYS@users.noreply.github.com>
2018-05-29 15:33:11 +0300

    hw4 test case2 added.

---
+++ src/main/java/pageObjects/HomeWork4.java
@@ -0,0 +1,108 @@
+package pageObjects;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Created by Mikhail on Май, 2018
+ */
+public class HomeWork4 {
+
+    @FindBy(css = ".profile-photo")
+    private WebElement userIcon;
+
+    @FindBy(css = "#Name")
+    private WebElement loginInput;
+
+    @FindBy(css = "#Password")
+    private WebElement passwordInput;
+
+    @FindBy(css = ".form-horizontal button[type = 'submit']")
+    private WebElement submitButton;
+
+    @FindBy(css = ".profile-photo span")
+    private WebElement profilePhoto;
+
+    @FindBy(css = ".dropdown")
+    private WebElement serviceDropDown;
+
+    @FindBy(css = "a[href = 'dates.html']")
+    private WebElement refToDatesPage;
+
+    @FindBy(css = "a[style^='left:']:first-child")
+    private WebElement leftSlider;
+
+    @FindBy(css = "a[style^='left:']:last-child")
+    private WebElement rightSlider;
+
+    @FindBy(css = ".info-panel-body-log > .info-panel-section")
+    private WebElement logRows;
+
+    public void open(WebDriver driver, String siteURL) {
+        driver.navigate().to(siteURL);
+    }
+
+    public void checkHomePageTitle(WebDriver driver, String title) {
+        assertEquals(driver.getTitle(), title);
+    }
+
+    public void login(String login, String password) {
+        userIcon.click();
+        loginInput.sendKeys(login);
+        passwordInput.sendKeys(password);
+        submitButton.click();
+    }
+
+    public void checkUserIsLoggined() {
+        assertEquals(profilePhoto.getText(), "PITER CHAILOVSKII");
+    }
+
+    public void openDatesPage() {
+        serviceDropDown.click();
+        refToDatesPage.click();
+    }
+
+    public void moveLeftSliderRange2(WebDriver driver, int position) {
+        Actions actions = new Actions(driver);
+        actions.click(leftSlider).build().perform();
+        String stringAttribute = leftSlider.getAttribute("style").replaceAll("\\D+", "");
+        int currentPosition = Integer.parseInt(stringAttribute);
+        if (position < currentPosition) {
+            for (int i = 0; i < currentPosition - position; i++) {
+                actions.sendKeys(Keys.ARROW_LEFT).build().perform();
+            }
+        } else {
+            for (int i = 0; i < position - currentPosition; i++) {
+                actions.sendKeys(Keys.ARROW_RIGHT).build().perform();
+            }
+        }
+        System.out.println("Ola" + leftSlider.getAttribute("style"));
+    }
+
+    public void moveRightSliderRange2(WebDriver driver, int position) {
+        Actions actions = new Actions(driver);
+        actions.click(rightSlider).build().perform();
+        String stringAttribute = rightSlider.getAttribute("style").replaceAll("\\D+", "");
+        int currentPosition = Integer.parseInt(stringAttribute);
+        if (position < currentPosition) {
+            for (int i = 0; i < currentPosition - position; i++) {
+                actions.sendKeys(Keys.ARROW_LEFT).build().perform();
+            }
+        } else {
+            for (int i = 0; i < position - currentPosition; i++) {
+                actions.sendKeys(Keys.ARROW_RIGHT).build().perform();
+            }
+        }
+
+    }
+
+    public void checkLogRowsForSliders(String haveToContains) {
+        assertTrue(logRows.getText().contains(haveToContains));
+    }
+}
