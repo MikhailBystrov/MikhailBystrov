6ef2a1599d720b2a6927f79400c03c5f0ab6d452
MIXSTAYS <32819223+MIXSTAYS@users.noreply.github.com>
2018-06-04 14:12:50 +0300

    lessons classes updated

---
+++ src/main/java/lessons/runners/CucumberTestngRunner.java
@@ -0,0 +1,19 @@
+package lessons.runners;
+
+import com.codeborne.selenide.Configuration;
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.BeforeSuite;
+
+/**
+ * Created by Danila_Morokov on 5/30/2018.
+ */
+
+@CucumberOptions(features = "src/test/java/lesson6", glue = "pageObjects")
+public class CucumberTestngRunner extends AbstractTestNGCucumberTests {
+
+    @BeforeSuite
+    public void beforeSuite() {
+        Configuration.browser = "chrome";
+    }
+}
